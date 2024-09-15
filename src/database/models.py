6d76f5eee4935fe9f97c43d67a88cf457e4dd91a
DDjog <dorota_drozdz@o2.pl>
2024-09-15 15:05:51 +0200

    changes reagrding relationships

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy import Column, Integer, String, Table, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
@@ -20,52 +20,57 @@ book_m2m_tag = Table(
     Column("tag_id", Integer, ForeignKey("tags.id", ondelete="CASCADE")),
                         )
 
+
 class Book(Base):
     __tablename__ = "books"
     id = Column(Integer, primary_key=True)
     title = Column(String(50), nullable=True)
-    isbn = Column(Integer, nullable=True, unique=True)
-    cover_page = Column(String, nullable=True)
-    author = relationship("Author", backref="authors")
-    tag = relationship("Tag", backref="books")
+    isbn = Column(String(13), nullable=True, unique=True)
+    cover_page = Column(LargeBinary, nullable=True)
+    author = relationship("Author", secondary=book_m2m_author, backref="books")
+    tag = relationship("Tag", secondary=book_m2m_tag, backref="books")
+    publisher_id = Column(Integer, ForeignKey("publishers.id"))
+    publisher = relationship("Publisher", backref="books")
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("BookCategory", backref="books")
+    shelf_signature_id = Column(Integer, ForeignKey("shelf_signatures.id"))
+    shelf_signature = relationship("BookShelfSignature", backref="books")
 
 class Language(Base):
     __tablename__ = "languages"
     id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey("books.id", ondelete="CASCADE"))
-    language = Column(String(50))
+    language = Column(String(50)
+
 
 class Author(Base):
     __tablename__ = "authors"
     id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey("books.id", ondelete="CASCADE"))
     author_name = Column(String(50), nullable=True)
     author_surname = Column(String(50), nullable=True)
 
+
 class Publisher(Base):
-    __tablename__ = "publisher"
+    __tablename__ = "publishers"
     id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey("books.id", ondelete="CASCADE"))
     publisher = Column(String(150), nullable=True, unique=False)
     publication_year = Column(Integer, nullable=True, unique=False)
+
 
 class Tag(Base):
     __tablename__ = "tags"
     id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey("books.id", ondelete="CASCADE"))
     tag = Column(String(50), nullable=True, unique=False, default=None)
 
-class BookCathegory(Base):
-    __tablename__ = "cathegories"
-    id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey("books.id", ondelete="CASCADE"))
-    name = Column(String(50), nullable=True, unique=False)
 
-class BookLocalisation(Base):
-    __tablename__ = "localisations"
+class BookCategory(Base):
+    __tablename__ = "categories"
     id = Column(Integer, primary_key=True)
-    cathegory_id = Column(Integer, ForeignKey("cathegories.id", ondelete="CASCADE"))
-    publisher = Column(String(150), nullable=True, unique=False)
-    bookshelf_no = Column(Integer, nullable=True, unique=False)
+    category_name = Column(String(50), nullable=True, unique=False)
+
+
+class BookShelfSignature(Base):
+    __tablename__ = "shelf_signatures"
+    id = Column(Integer, primary_key=True)
+    shelf_code = Column(String(50), nullable=True, unique=False)
 
 
