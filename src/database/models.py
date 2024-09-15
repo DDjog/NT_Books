ed828466889b80108c13d98fb2649d66bed65252
DDjog <dorota_drozdz@o2.pl>
2024-09-15 15:40:40 +0200

    chnages in alembic

--- src/database/models.py
+++ src/database/models.py
@@ -39,7 +39,7 @@ class Book(Base):
 class Language(Base):
     __tablename__ = "languages"
     id = Column(Integer, primary_key=True)
-    language = Column(String(50)
+    language = Column(String(50))
 
 
 class Author(Base):
